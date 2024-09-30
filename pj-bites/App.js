2bea893e3e74052cefb449725be91d7bae4729ee
Tomas <96405@sistemas.frc.utn.edu.ar>
2024-09-30 16:22:04 -0300

    esqueleto

---
+++ pj-bites/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Bites en proceso</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f8d3cf',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
