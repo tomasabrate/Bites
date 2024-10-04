f8f6b0b0f510e1c6678fc4192fd70c19edd5824c
Alexis Carnero <69685158+alexisjcarnero@users.noreply.github.com>
2024-10-03 21:50:08 -0300

    Se conecto Front y Back mediante "cors". Se instalo el modulo cors(Cross-Origin Resource Sharing) en el backend y se implemento un "fetch()" en el archivo "Productos.jsx" del frontend para obtener los productos cargados en la database.

--- Frontend/pj-bites/screens/Productos.jsx
+++ Frontend/pj-bites/screens/Productos.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { useEffect, useState } from "react";
+import { View, Text, FlatList } from "react-native";
 
 export default function Productos() {
+  const [productos, setProductos] = useState([]);
+
+  // Función para obtener productos del backend
+  const obtenerProductos = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/productos"); //Aqui tiene que ir el dominio o puerto donde tengas corriendo el back.
+      const data = await response.json();
+      setProductos(data);
+    } catch (error) {
+      console.error("Error al obtener productos:", error);
+    }
+  };
+
+  // Ejecutar la función para obtener productos al cargar la pantalla
+  useEffect(() => {
+    obtenerProductos();
+  }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#ffeae6' }}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#ffeae6",
+      }}
+    >
       <Text>Productos!</Text>
+      <FlatList
+        data={productos}
+        keyExtractor={(item) => item.id_producto.toString()}
+        renderItem={({ item }) => <Text>{item.nombre}{}</Text>}
+      />
     </View>
   );
 }
