8ee2aa04f412629e301da4584eb647382fe42e1e
caroMsoria43 <caromiksor3@gmail.com>
2024-10-05 14:02:41 -0300

    Agregado el formulario de comercio y cliente con validaciones y estilos

--- Frontend/pj-bites/components/ZorritoForm.jsx
+++ Frontend/pj-bites/components/ZorritoForm.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Alert, ScrollView } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import ZorritoSelector from './zorritoSelector'; // Asegúrate de ajustar la ruta
+import ClientTermsModal from './TermsModalCliente'; // Importa el modal de términos
 
 const categories = ['Postres', 'Comida Saludable', 'Bebidas', 'Viandas', 'Comida Rápida'];
 
@@ -13,7 +14,9 @@ const ZorritoForm = ({ onSubmit }) => {
   const [birthDate, setBirthDate] = useState(new Date());
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [selectedCategories, setSelectedCategories] = useState([]);
-  const [selectedZorrito, setSelectedZorrito] = useState(null); // Estado para el zorrito seleccionado
+  const [selectedZorrito, setSelectedZorrito] = useState(null);
+  const [termsAccepted, setTermsAccepted] = useState(false); // Estado para los términos
+  const [showTermsModal, setShowTermsModal] = useState(false); // Estado para el modal
 
   const handleCategorySelect = (category) => {
     if (selectedCategories.includes(category)) {
@@ -24,7 +27,6 @@ const ZorritoForm = ({ onSubmit }) => {
   };
 
   const handleSubmit = () => {
-    // Validación de campos
     if (!fullName || !email || !phoneNumber || !address || !selectedZorrito) {
       Alert.alert('Error', 'Por favor completa todos los campos.');
       return;
@@ -37,19 +39,19 @@ const ZorritoForm = ({ onSubmit }) => {
       address,
       birthDate: birthDate.toLocaleDateString(),
       preferences: selectedCategories,
-      profilePicture: selectedZorrito, // Añade el zorrito seleccionado al formulario
+      profilePicture: selectedZorrito,
     };
-    
+
     onSubmit(formData);
     Alert.alert('Formulario Enviado', JSON.stringify(formData));
-    
-    // Reiniciar el formulario
+
     setFullName('');
     setEmail('');
     setPhoneNumber('');
     setAddress('');
     setSelectedCategories([]);
-    setSelectedZorrito(null); // Reiniciar la imagen seleccionada
+    setSelectedZorrito(null);
+    setTermsAccepted(false); // Reinicia la aceptación de términos
   };
 
   const showDatePickerModal = () => {
@@ -65,82 +67,109 @@ const ZorritoForm = ({ onSubmit }) => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>¡Completa tu Perfil!</Text>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <Text style={styles.title}>¡Completa tu Perfil!</Text>
 
-      <ZorritoSelector onSelect={setSelectedZorrito} /> {/* Componente ZorritoSelector */}
+        <ZorritoSelector onSelect={setSelectedZorrito} />
 
-      <Text style={styles.label}>Nombre Completo</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Ej. Juan Pérez"
-        value={fullName}
-        onChangeText={setFullName}
-      />
-
-      <Text style={styles.label}>Correo Electrónico</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Ej. correo@dominio.com"
-        keyboardType="email-address"
-        value={email}
-        onChangeText={setEmail}
-      />
-
-      <Text style={styles.label}>Número de Teléfono</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Ej. 555-1234567"
-        keyboardType="phone-pad"
-        value={phoneNumber}
-        onChangeText={setPhoneNumber}
-      />
-
-      <Text style={styles.label}>Domicilio</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Ej. Calle 123, Ciudad"
-        value={address}
-        onChangeText={setAddress}
-      />
-
-      <Text style={styles.label}>Fecha de Nacimiento</Text>
-      <TouchableOpacity style={styles.input} onPress={showDatePickerModal}>
-        <Text style={styles.dateText}>{birthDate.toLocaleDateString()}</Text>
-      </TouchableOpacity>
-      {showDatePicker && (
-        <DateTimePicker
-          value={birthDate}
-          mode="date"
-          display="default"
-          onChange={onChange}
+        <Text style={styles.label}>Nombre Completo</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Ej. Juan Pérez"
+          value={fullName}
+          onChangeText={setFullName}
         />
-      )}
 
-      <Text style={styles.label}>Preferencias Alimentarias</Text>
-      <View style={styles.categoriesContainer}>
-        {categories.map((category) => (
-          <TouchableOpacity
-            key={category}
-            style={[
-              styles.categoryButton,
-              selectedCategories.includes(category) && styles.selectedCategory,
-            ]}
-            onPress={() => handleCategorySelect(category)}
-          >
-            <Text style={styles.categoryText}>{category}</Text>
+        <Text style={styles.label}>Correo Electrónico</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Ej. correo@dominio.com"
+          keyboardType="email-address"
+          value={email}
+          onChangeText={setEmail}
+        />
+
+        <Text style={styles.label}>Número de Teléfono</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Ej. 555-1234567"
+          keyboardType="phone-pad"
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+        />
+
+        <Text style={styles.label}>Domicilio</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Ej. Calle 123, Ciudad"
+          value={address}
+          onChangeText={setAddress}
+        />
+
+        <Text style={styles.label}>Fecha de Nacimiento</Text>
+        <TouchableOpacity style={styles.input} onPress={showDatePickerModal}>
+          <Text style={styles.dateText}>{birthDate.toLocaleDateString()}</Text>
+        </TouchableOpacity>
+        {showDatePicker && (
+          <DateTimePicker
+            value={birthDate}
+            mode="date"
+            display="default"
+            onChange={onChange}
+          />
+        )}
+
+        <Text style={styles.label}>Preferencias Alimentarias</Text>
+        <View style={styles.categoriesContainer}>
+          {categories.map((category) => (
+            <TouchableOpacity
+              key={category}
+              style={[
+                styles.categoryButton,
+                selectedCategories.includes(category) && styles.selectedCategory,
+              ]}
+              onPress={() => handleCategorySelect(category)}
+            >
+              <Text style={styles.categoryText}>{category}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+        <View style={styles.termsContainer}>
+          <TouchableOpacity onPress={() => setShowTermsModal(true)}>
+            <Text style={styles.termsText}>Leer Términos y Condiciones</Text>
           </TouchableOpacity>
-        ))}
+          <TouchableOpacity
+            style={styles.checkboxContainer}
+            onPress={() => setTermsAccepted(!termsAccepted)} // Cambia el estado al hacer clic
+          >
+            <View style={[styles.checkbox, termsAccepted && styles.checkedCheckbox]} />
+            <Text style={styles.checkboxText}>He leído y acepto los términos y condiciones</Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity
+          style={[styles.submitButton, !termsAccepted && styles.disabledButton]}
+          onPress={termsAccepted ? handleSubmit : null}
+          disabled={!termsAccepted}
+        >
+          <Text style={styles.submitButtonText}>Registrar</Text>
+        </TouchableOpacity>
       </View>
 
-      <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
-        <Text style={styles.submitButtonText}>Registrar</Text>
-      </TouchableOpacity>
-    </View>
+      {/* Modal de términos y condiciones */}
+      <ClientTermsModal visible={showTermsModal} onClose={() => setShowTermsModal(false)} />
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     padding: 20,
     backgroundColor: 'white',
@@ -150,10 +179,8 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
-    width: '90%',
-    alignSelf: 'center',
+    width: '90%', // Asegúrate de que esto esté correcto
     marginTop: 20,
-    minHeight: 600,
   },
   title: {
     fontSize: 28,
@@ -203,12 +230,46 @@ const styles = StyleSheet.create({
     color: '#333',
     fontSize: 16,
   },
+  termsContainer: {
+    marginVertical: 15,
+    alignItems: 'center',
+  },
+  termsText: {
+    color: '#007bff',
+    fontSize: 16,
+    textDecorationLine: 'underline',
+    marginBottom: 5,
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  checkbox: {
+    width: 24,
+    height: 24,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginRight: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  checkedCheckbox: {
+    backgroundColor: '#4caf50', // Color cuando está seleccionado
+  },
+  checkboxText: {
+    marginLeft: 5,
+    fontSize: 16,
+  },
   submitButton: {
     backgroundColor: '#ff6347',
     paddingVertical: 15,
     borderRadius: 10,
     alignItems: 'center',
     marginTop: 20,
+  },
+  disabledButton: {
+    backgroundColor: 'lightgray',
   },
   submitButtonText: {
     color: 'white',
