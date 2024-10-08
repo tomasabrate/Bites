0f732d37c0cc3e146b2b3310f654789ea77e2a90
Alexis Carnero <69685158+alexisjcarnero@users.noreply.github.com>
2024-10-07 21:50:15 -0300

    Se creo un boton temporal 'Inicio Developer'.

--- Frontend/pj-bites/navigation/AppNavigator.jsx
+++ Frontend/pj-bites/navigation/AppNavigator.jsx
@@ -1,51 +1,38 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import SplashScreen from '../screens/SplashScreen';
-import IntroScreen from '../screens/IntroScreen';
-import LoginSelectionScreen from '../screens/LoginSelectionScreen';
-import LoginScreen from '../screens/LoginScreen';
-import ClienteProfile from '../screens/ClienteProfile';
-import ComercioProfile from '../screens/ComercioProfile';
-import Perfil from '../screens/Perfil';
-import Locales from '../screens/LocalesScreen'; // Asegúrate de importar la nueva pantalla
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import SplashScreen from "../screens/SplashScreen";
+import IntroScreen from "../screens/IntroScreen";
+import LoginSelectionScreen from "../screens/LoginSelectionScreen";
+import LoginScreen from "../screens/LoginScreen";
+import ClienteProfile from "../screens/ClienteProfile";
+import ComercioProfile from "../screens/ComercioProfile";
+import Perfil from "../screens/Perfil";
+import Productos from "../screens/Productos";
+import Locales from "../screens/LocalesScreen"; // Asegúrate de importar la nueva pantalla
 
 const Stack = createStackNavigator();
 
 export default function AppNavigator() {
   return (
     <Stack.Navigator initialRouteName="Splash">
-      <Stack.Screen 
-        name="Splash" 
-        component={SplashScreen} 
-        options={{ headerShown: false }} 
+      <Stack.Screen
+        name="Splash"
+        component={SplashScreen}
+        options={{ headerShown: false }}
       />
-      <Stack.Screen 
-        name="Intro" 
-        component={IntroScreen} 
-        options={{ headerShown: false }} 
+      <Stack.Screen
+        name="Intro"
+        component={IntroScreen}
+        options={{ headerShown: false }}
       />
-      <Stack.Screen 
-        name="LoginSelection" 
-        component={LoginSelectionScreen} 
-      />
-      <Stack.Screen 
-        name="Login" 
-        component={LoginScreen} 
-      />
-      <Stack.Screen 
-        name="ClienteProfile" 
-        component={ClienteProfile} 
-      />
-      <Stack.Screen 
-        name="ComercioProfile" 
-        component={ComercioProfile} 
-      />
-      <Stack.Screen 
-        name="Perfil" 
-        component={Perfil} 
-      />
-      <Stack.Screen 
-        name="Locales" 
+      <Stack.Screen name="Productos" component={Productos} />
+      <Stack.Screen name="LoginSelection" component={LoginSelectionScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="ClienteProfile" component={ClienteProfile} />
+      <Stack.Screen name="ComercioProfile" component={ComercioProfile} />
+      <Stack.Screen name="Perfil" component={Perfil} />
+      <Stack.Screen
+        name="Locales"
         component={Locales} // Agrega esta línea
       />
     </Stack.Navigator>
