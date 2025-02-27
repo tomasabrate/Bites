3558c4fcc82d397b6977b95c6894d5ea8f8496b2
Tomas <96405@sistemas.frc.utn.edu.ar>
2025-02-27 17:28:39 -0300

    .

--- Backend/src/database/connection.js
+++ Backend/src/database/connection.js
@@ -1,8 +1,14 @@
 import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER, DB_PORT } from '../config.js';
 import mysql from "mysql2/promise"; //IMPORTANTE agregar el "/promise" que es lo que nos permite usar promesas async await
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_DATABASE:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+
 export const pool = mysql.createPool({
-  host: DB_HOST, //si tuvieramos algun servicio en la nube, aqui iria la IP
+  host: DB_HOST, 
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
