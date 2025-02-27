63683acca005ca0e21172546d4ef6f163d4231e9
Tomas <96405@sistemas.frc.utn.edu.ar>
2025-02-27 17:43:02 -0300

    .

--- Backend/src/database/connection.js
+++ Backend/src/database/connection.js
@@ -4,7 +4,7 @@ import mysql from "mysql2/promise"; //IMPORTANTE agregar el "/promise" que es lo
 console.log("DB_HOST:", process.env.DB_HOST);
 console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_DATABASE:", process.env.DB_HOST);
+console.log("DB_DATABASE:", process.env.DB_DATABASE);
 console.log("DB_PORT:", process.env.DB_PORT);
 
 export const pool = mysql.createPool({
