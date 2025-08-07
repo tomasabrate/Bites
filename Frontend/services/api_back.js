4da4c044a0265a7cab6698c1784edafa4701e573
Tomas <96405@sistemas.frc.utn.edu.ar>
2025-08-06 21:24:16 -0300

    api en servidor

--- Frontend/services/api_back.js
+++ Frontend/services/api_back.js
@@ -1,9 +1,9 @@
 //no se si se configura con un archivo .env
 
 //direccion del backend en Railway, inactiva para no gastar recursos pero se puede activar
-//export const API_URL_BACK = 'https://bites-production.up.railway.app';
+export const API_URL_BACK = 'https://bites-production.up.railway.app';
 //esta URL es la que le damos a nuestro marketplace de Mercado Pago para que nos notifique.
 //export const API_URL_BACK = 'https://6sd6lg81-3000.brs.devtunnels.ms';
 
 //direccion del backend en localhost para desarrollo
-export const API_URL_BACK = 'http://localhost:3000';
+//export const API_URL_BACK = 'http://localhost:3000';
